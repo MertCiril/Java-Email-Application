d724726de769fa993d01ab70da60abb8e75e60fb
MertCiril <cirilmert@gmail.com>
2022-05-17 19:32:33 +0300

    Java-Email Application

---
+++ email.application/src/email/application/Email.java
@@ -0,0 +1,64 @@
+package email.application;
+
+import java.util.Scanner;
+
+public class Email {
+    private String firstName,lastName,password,department,alternateEmail,email;
+    private int mailboxCapacity=500,defaultPasswordLength=10;
+    private String companySuffix="aeycompany.com";
+    
+    public Email(String firstName,String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        
+        this.department=setDepartment();
+        
+        this.password=randomPassword(defaultPasswordLength);
+        System.out.println("Your password is:"+this.password);
+        
+        email=firstName.toLowerCase()+"."+lastName.toLowerCase()+"@"+department+companySuffix;
+
+    }
+    
+    private String setDepartment(){
+        System.out.println("New worker:"+firstName+"Department Codes\n1 for Sales \n2 bfor Devekopment\n3 for Acoount\n0 for none");
+        System.out.print("Enter department code:");
+        Scanner in=new Scanner(System.in);
+        int depChoice=in.nextInt();
+        if(depChoice==1){return "sales";}
+        else if(depChoice==2){return "dev";}
+        else if(depChoice==3){return "acct";}
+        else {return"";}
+    }
+    
+    private String randomPassword(int length){
+        String passwordSet="ABCDEFGHIJKLMNOPQRSTUWXYZ01234567891@^#$%";
+        char[]password=new char[length];
+        
+        for(int i=0;i<length;i++)
+        {
+            int rand =(int)(Math.random()*passwordSet.length());
+            password[i]=passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
+    public void setMailboxCapacity(int capacity){
+        this.mailboxCapacity=capacity;
+    }
+    public void setAlternateEmail(String altEmail){
+        this.alternateEmail=altEmail;
+    }
+    public void changePassword(String password){
+        this.password=password;
+    }
+    public int getMailboxCapacity(){return mailboxCapacity;}
+    public String getAlternateEmail(){return alternateEmail;}
+    public String getPassword(){return password;}
+    
+    public String showInfo(){
+        return "DISPLAY NAME:"+firstName+" "+lastName+
+                "\nCOMPANY EMAIL:"+email+
+                "\nMAILBOX CAPACITY:"+mailboxCapacity+"mb";
+    }
+
+}
